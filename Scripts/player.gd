ea1d4cd032ea7a58894601a640ee1a0937cef91f
Kevin Wilson <ironmonkeyking85@gmail.com>
2023-11-13 15:50:07 -0600

    state code

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,28 +1,18 @@
+class_name Player
 extends CharacterBody2D
 
+var speed: float = 100.0
+var buoyancy: float = 100.0
+var gravity: float = 100.0
+var pressure: float = 100.0
+var propulsion: float = 100.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+enum States {MOVING, SHOOTING, DAMAGED, DISTORYED}
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _physics_process(delta: float) -> void:
+	
 
 	move_and_slide()
