f2698ee6007bf33351d40080adb6c6b068054156
Kevin Wilson <ironmonkeyking85@gmail.com>
2023-11-13 19:50:57 -0600

    working player controls

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,7 @@
 class_name Player
 extends CharacterBody2D
 
-var speed: float = 100.0
+
 var buoyancy: float = 100.0
 var gravity: float = 100.0
 var pressure: float = 100.0
@@ -12,7 +12,21 @@ enum States {MOVING, SHOOTING, DAMAGED, DISTORYED}
 
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
+	velocity.x = 0
+	velocity.y = 0
 	
-
+	if Input.is_action_pressed("Right"):
+		velocity.x += 1
+		
+	if Input.is_action_pressed("Left"):
+		velocity.x -= 1
+		
+	if Input.is_action_pressed("Up"):
+		velocity.y -= 1
+		
+	if Input.is_action_pressed("Down"):
+		velocity.y += 1			
+	
+	velocity *= propulsion
 	move_and_slide()
