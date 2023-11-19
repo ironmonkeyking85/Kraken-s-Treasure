e2ca08f1f231cdd821dd8b3e06b83bd6e1dbc4f1
Kevin Wilson <ironmonkeyking85@gmail.com>
2023-11-18 20:04:19 -0600

    state

--- Scripts/State Machine/Moving.gd
+++ Scripts/State Machine/Moving.gd
@@ -1,12 +1,28 @@
 class_name Moving_State
 extends State
 
+@export var player_movement: Player
+@export var animation: AnimationPlayer
+@export var velocity = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
+
+signal player_actions
+
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(false)
 
+func enter() -> void:
+	set_physics_process(true)
+	#animation.play("moving")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func exit() -> void:
+	set_physics_process(false) 	
+
+func _physics_process(delta):
+	pass	
+	
+	
+
+	
+	
+
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,37 @@
 class_name Player
 extends CharacterBody2D
 
+@export var buoyancy: float = 100.0
+@export var gravity: float = 100.0
+@export var pressure: float = 100.0
+@export var propulsion: float = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
+	Globals.Player = self
+
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta: float)-> void:
+	velocity.x = 0
+	velocity.y = 0
+
+	if Input.is_action_pressed("Right"):
+		velocity.x += 1
+
+	if Input.is_action_pressed("Left"):
+		velocity.x -= 1
+
+	if Input.is_action_pressed("Up"):
+		velocity.y -= 1
+
+	if Input.is_action_pressed("Down"):
+		velocity.y += 1			
+
+	velocity *= propulsion
+	move_and_slide()
+
+
+
