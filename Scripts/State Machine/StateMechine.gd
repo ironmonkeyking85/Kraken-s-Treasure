3447b83f4312bc7b0f2051668932c240f5b804da
Kevin Wilson <ironmonkeyking85@gmail.com>
2023-11-24 08:18:44 -0600

    state machine templet
    update for state machine and state files

--- Scripts/State Machine/StateMechine.gd
+++ Scripts/State Machine/StateMechine.gd
@@ -1,15 +1,78 @@
 class_name StateMachine
 extends Node
 
-enum PlAYERSTATES { IDLE, ATTACK, MOVING, DAMAGED, DISTORYED }
+# Enumeration for player states
+enum PlAYERSTATES { IDLE, ATTACK, MOVING, DAMAGED, DISTORYED, INVALID }
 
-@export var state: State
+# Exported variables for the character and initial state.
+# These variables are exposed in the editor, allowing the user to assign values.
+@export var character : CharacterBody2D = null
+@export var initialestate : State = null
 
-func _ready():
-	change_state(state)
-								
-func change_state(new_state:State):
-	if state is State:
-		state.exit()
-	new_state.enter()
-	state = new_state	
+# Variable to store the current state of the player.
+# This variable is used to keep track of the current state in the state machine.
+var current_state : State = null
+ 
+# Function called when the node is ready
+# This function is automatically called when the node is ready.
+# It initializes the states by calling their 'Initialize' function,
+# passing the character reference. It then sets the current state to
+# the initial state and calls the 'change_state' function to enter
+# the initial state.
+func _ready() -> void:
+# Loop through each child node of the StateMachine node.
+	for child in get_children():
+# Cast the child node to the State class.		
+		var state_node : State = child as State
+# Initialize the state node with the character reference.		
+		state_node.Initialize(character)
+
+# Set the current state to the initial state.	
+	current_state = initialestate
+# Call the 'change_state' function to enter the initial state.	
+	change_state(initialestate)	
+
+# Function to handle input events and transition between states	
+# This function is called in response to input events.
+# It delegates the input handling to the current state's 'input' function,
+# which returns the next state. If the next state is valid, it changes
+# the current state to the new state using the 'change_state' function.
+func _input(event : InputEvent) -> void:
+# Get the next state based on the input event.	
+	var nextstate : PlAYERSTATES = current_state.input(event)
+# Check if the next state is valid.	
+	if (nextstate != PlAYERSTATES.INVALID):
+# Change the current state to the new state.		
+		change_state(get_child(nextstate))
+		
+# Function for the main process, handling state transitions
+# This function is called during the main processing loop.
+# It delegates the main update to the current state's 'main_update' function,
+# which returns the next state. If the next state is valid, it changes
+# the current state to the new state using the 'change_state' function.		
+func _process(delta: float) -> void:	
+	var nextstate : PlAYERSTATES = current_state.main_update(delta)
+	if (nextstate != PlAYERSTATES.INVALID):
+		change_state(get_child(nextstate))
+		
+# Function for physics process, handling state transitions	
+# This function is called during the physics processing loop.
+# It delegates the physics update to the current state's 'physics_update' function,
+# which returns the next state. If the next state is valid, it changes
+# the current state to the new state using the 'change_state' function.	
+func _physics_process(delta: float) -> void:
+	var nextstate : PlAYERSTATES = current_state.physics_update(delta)
+	if (nextstate != PlAYERSTATES.INVALID):
+		change_state(get_child(nextstate))
+
+# Function to change the current state
+# This function is responsible for changing the current state.
+# It calls the 'exit' function of the current state, then updates
+# the current state to the new state and calls its 'enter' function.		
+func change_state(nextstate : State) -> void:
+	current_state.exit()
+	current_state = nextstate
+	current_state.enter()		
+			
+		
+		
--- Scripts/State Machine/state.gd
+++ Scripts/State Machine/state.gd
@@ -1,36 +1,61 @@
 class_name State
 extends Node
 
+ # Constants for the names of animation and audio nodes
 const character_animation_name: String = "Animation"
 const character_aduio_name: String = "Audio"
 
+# Exported variables for the state and animation names
 @export var state_name : String = ""
 @export var animation_name : String = ""
 
+# References to the character body, animation player, and audio nodes
 var characterbody_reference : CharacterBody2D = null
 var animation_reference : AnimationPlayer = null
 var audio_reference : Node = null
 
+# Function to initialize the state with a character body
+# This function is called to set up the state with the necessary references,
+# including the character body, animation player, and audio nodes.
 func Initialize( character_body : CharacterBody2D ) -> void:
 	characterbody_reference = character_body
+# Get the animation player node from the character body.	
 	animation_reference = character_body.get_node(character_animation_name)
+# Get the audio node from the character body.	
 	audio_reference = character_body.get_node(character_aduio_name)
 
+# Function called when entering the state
+# This function is invoked when the state is entered.
+# It plays the specified animation using the animation player.
 func enter() -> void:
 	animation_reference.play(animation_name)
-	
+
+# Function called when exiting the state
+# This function is called when the state is exited.
+# It currently does nothing (placeholder pass statement).	
 func exit() -> void:
 	pass
 	
-func input(event : InputEvent) -> StateMachine.StateType:
-	return StateMachine.StateType.Invalid
-
-func main_update(_delta : float) -> StateMachine.StateType:
-	return StateMachine.StateType.Invalid
+# Function to handle input events and transition between states
+# This function is responsible for handling input events and determining
+# the next state based on the received input. In this case, it always returns
+# the INVALID state, signaling that the state transition is not determined by input.
+func input(event : InputEvent) -> StateMachine.PlAYERSTATES:
+	return StateMachine.PlAYERSTATES.INVALID
 	
-
-func physics_update(_delta: float) -> StateMachine.StateType:
-	return StateMachine.StateType.Invalid
+# Function for main updates, returning the next state
+# These functions are responsible for updating the state during the main and
+# physics processing loops. In this case, both functions always return the
+# INVALID state, indicating that the state transition is not determined by updates.
+func main_update(_delta : float) -> StateMachine.PlAYERSTATES:
+	return StateMachine.PlAYERSTATES.INVALID
+	
+# Function for physics updates, returning the next state
+# Similar to main_update, these functions handle updates during the physics processing.
+# They also always return the INVALID state, suggesting that physics updates do not
+# determine state transitions in this context.
+func physics_update(_delta: float) -> StateMachine.PlAYERSTATES:
+	return StateMachine.PlAYERSTATES.INVALID
 
 
 
