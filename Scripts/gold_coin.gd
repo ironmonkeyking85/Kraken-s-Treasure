d91d9fca068bb80cac1d84d61d9fd942b89544e7
Kevin Wilson <ironmonkeyking85@gmail.com>
2023-12-06 20:12:19 -0600

    enemy path setup

--- Scripts/gold_coin.gd
+++ Scripts/gold_coin.gd
@@ -8,7 +8,6 @@ extends Node2D
 func _on_coin_pick_up_body_entered(body):
 	if body.name == "Player":
 		Globals.gold_coins += 1
-		print(Globals.gold_coins)
 		$AnimationPlayer.play("Coin_picked_up")
 		await $AnimationPlayer.animation_finished
 		queue_free()
