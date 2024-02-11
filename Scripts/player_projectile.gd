a1381dcdefda2a8545e93cf32bc5cd5c2273e023
Kevin Wilson <ironmonkeyking85@gmail.com>
2024-02-11 13:50:00 -0600

    projectile Vfx

--- Scripts/player_projectile.gd
+++ Scripts/player_projectile.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+@onready var contact_paticales = $ContactParticales
+@onready var timer = $Timer
 @export var speed: float = 1000.0
 var velocity = Vector2.ZERO
 
+func _ready():
+	contact_paticales.emitting = false
 
 func start(_transform):
 	transform = _transform
@@ -15,15 +19,26 @@ func _process(delta):
 			
 func _on_projectile_collision_body_entered(body):
 	if body.is_in_group("Collidable"):
-		queue_free()
-		
+		contact_paticales.emitting = true
+		timer.start()
+			
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 		
 func _on_projectile_collision_area_entered(area):
-	if area.is_in_group("shootable_enemy"):
-		queue_free()
-	if area.is_in_group("Interactable_Hazards"):
-		queue_free()
+	if area.is_in_group("Shootable_enemy"):
+		contact_paticales.emitting = true
+		timer.start()
 		
+	elif area.is_in_group("Interactable_Hazards"):
+		contact_paticales.emitting = true
+		timer.start()
+		
+	elif area.is_in_group("WallCollision"):
+		contact_paticales.emitting = true
+		timer.start()	
+		
+func _on_timer_timeout():
+	queue_free()
+	
 
